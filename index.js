97425878752207a91422edf7291ae79f41aab0a0
Romie Greenbourg <39065949+informathemusic@users.noreply.github.com>
2020-12-28 10:33:23 +0100

    Back to normal, if i'm not wrong

--- index.js
+++ index.js
@@ -20,6 +20,30 @@ class Db {
     if (!settings.exportThis || settings.exportThis == null) return this.value;
   }
 
+  genProxy(data) {
+    return new Proxy(data, {
+      set: (obj, prop, val) => {
+        obj[prop] = val;
+        if (this.saveOnChange) {
+          this.update();
+        }
+        return true;
+      },
+      deleteProperty: (obj, prop) => {
+        delete obj[prop];
+        if (this.saveOnChange) {
+          this.update();
+        }
+        return true;
+      },
+      get: (obj, prop) => (
+        typeof obj[prop] === 'object' && obj[prop]
+          ? this.genProxy(obj[prop])
+          : obj[prop]
+      ),
+    });
+  }
+
   /**
    * async Updates the file/db from this.readOnlyValue
    * @returns {any}  - The json file
--- utils.js
+++ utils.js
@@ -3,27 +3,4 @@ module.exports = {
     Object.keys(db).forEach((k) => delete db[k]);
     Object.keys(setTo).forEach((k) => { db[k] = setTo[k]; });
   },
-  genProxy(data) {
-    return new Proxy(data, {
-      set: (obj, prop, val) => {
-        obj[prop] = val;
-        if (this.saveOnChange) {
-          this.update();
-        }
-        return true;
-      },
-      deleteProperty: (obj, prop) => {
-        delete obj[prop];
-        if (this.saveOnChange) {
-          this.update();
-        }
-        return true;
-      },
-      get: (obj, prop) => (
-        typeof obj[prop] === 'object' && obj[prop]
-          ? this.genProxy(obj[prop])
-          : obj[prop]
-      ),
-    });
-  },
 };
